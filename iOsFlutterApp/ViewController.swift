ba58aa8dce94fba52453011681dd8d50d107ec7b
Ljay Arreza <ljarreza9@gmail.com>
2024-03-11 14:30:02 +0800

    Initial Commit

---
+++ iOsFlutterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOsFlutterApp
+//
+//  Created by Louie Jay Arreza on 3/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
